5dcd0c620c2d77de787ae1c4a5b460c2a39432db
lovesharma2005 <122599088+lovesharma2005@users.noreply.github.com>
2023-01-13 14:59:42 +0530

    Add files via upload

---
+++ perimeter_rectangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int l,b;
+
+    printf ("Enter the length");
+    
+    scanf ("%d",&l);
+
+    printf ("Enter the breath");
+
+    scanf ("%d",&b);
+
+    printf ("perimeter of rectangle : %d",2*(l+b));
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int arr[5] = { 1, 2, 3, 4, 5 };
+int *ptr = arr;
+
+cout <<"\n"<< ptr;
+return 0;
+}
+
+// thus code is contributed by shivanisinghss2110
